b4951b08283d1165db24dfddc1864ee854a4c7d9
A00828073 <a00828073@itesm.mx>
2021-06-30 21:46:59 -0500

    Mejora en los comentarios del codigo

--- cannon.py
+++ cannon.py
@@ -1,21 +1,20 @@
-"""Cannon, hitting targets with projectiles.
+"""
 
-Exercises
-
-1. Keep score by counting target hits.
-2. Vary the effect of gravity.
-3. Apply gravity to the targets.
-4. Change the speed of the ball.
+Cannon, hitting targets with projectiles.
 
 """
 
+# Librerias que van a estar siendo usadas
 from random import randrange
 from turtle import *
 
 from freegames import vector
 
+# Posicion inicial del balon
 ball = vector(-200, -200)
+# Vector de velocidad para el balon
 speed = vector(0, 0)
+# Cosas a las que se le tiene que pegar
 targets = []
 
 
@@ -50,31 +49,42 @@ def draw():
 
 def move():
     "Move ball and targets."
+    # Ver si se va a agregar otro target
     if randrange(40) == 0:
+        # Posicion aletoria en el eje y
         y = randrange(-150, 150)
         target = vector(200, y)
+        # Agregarlo a la lista de proyectiles existente
         targets.append(target)
 
+    # Movimiento de los target en x
     for target in targets:
         target.x -= 0.5
 
+    # Generar movimiento del balon
     if inside(ball):
+        # Modificar el valor del vector de velocidad para simular tiro parabolico
         speed.y -= 0.35
         ball.move(speed)
 
     dupe = targets.copy()
     targets.clear()
 
+    # Poblar targets de solo targets que no han sido golpeados
     for target in dupe:
         if abs(target - ball) > 13:
             targets.append(target)
 
+    # Posicionar los elementos
     draw()
 
+    # Por cada target, ver si ya llego al borde de la izquierda
     for target in targets:
         if not inside(target):
+            # Si ese es el caso, posicionarlo en la salida derecha
             target.x = 200
 
+    # Repetir cada 1 milisegundo
     ontimer(move, 1)
 
 
